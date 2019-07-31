a07ea13b0f9d8b057d7943f62643d80c79796c67
George Kaimakis <geoptus@gmail.com>
2019-07-31 22:08:29 +0100

    Add fake 'posts' to test functionallity:
    	modified:   app/routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'George'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        {
+            'author': {'username': 'Meghan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Robert'},
+            'body': 'The auto-complete is blocking!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
