dbdffdd915d228567d578e86af93315c7d4cbef5
George Kaimakis <geoptus@gmail.com>
2019-08-03 13:15:34 +0100

    Adds table rename attribute - optional:
    Use this attribute if the class name is going to yield a
    hard-to-read/write table name, the attribute __tablename__ is generated
    using snakecase by default, ie
    class: AddressAndPhone -> tablename: address_and_phone
    
    	modified:   app/models.py

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 
 class User(db.Model):
     """User class forms the User database model setup"""
+    # __tablename__ = "prime_user" # optional rename of the table
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
