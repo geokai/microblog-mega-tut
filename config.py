d47f191cf24d78826a8c018e897681da13d55fb1
George Kaimakis <geoptus@gmail.com>
2019-08-03 11:05:27 +0100

    Adds docstring to Config class + a sml type:

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    """Configures the secret key, and the sql alchemy database"""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'splite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATION = False
