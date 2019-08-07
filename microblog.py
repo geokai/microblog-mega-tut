d4ae004b91d1ad43f4e60f5efbdbba785a1fad9f
George Kaimakis <geoptus@gmail.com>
2019-08-08 00:01:15 +0100

    Delete flask shell context function

--- microblog.py
+++ microblog.py
@@ -1,7 +1,2 @@
 from app import app, db     # noqa
 from app.models import User, Post   # noqa
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
