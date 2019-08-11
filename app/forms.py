6f55f1544cd51d2f2ba7d44f92bc795ac4a306cd
George Kaimakis <geoptus@gmail.com>
2019-08-11 14:36:07 +0100

    Add user login and resistration form:

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 
 class LoginForm(FlaskForm):
+    """Declare the Login form fields and implement validation."""
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign in')
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    """Declare the Registration form fields and implement validation."""
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        """Check if intended username is in user."""
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        """Check if intended email address is in use."""
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
